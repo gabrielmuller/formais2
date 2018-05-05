5c5423c3d4989b937b40045f03723c42d633d00d
root <gabrielmullerw@gmail.com>
2018-05-05 22:01:57 +0000

    Determinizar NFA

--- nfa.py
+++ nfa.py
@@ -1,4 +1,5 @@
 from misc import set_to_str
+from dfa import DFA
 class NFA:
     def __init__(self, transitions, initial, accepting):
         self.initial = initial
@@ -16,3 +17,59 @@ class NFA:
                     states = states.union(self.transitions[state][char])
         return list(filter(lambda s : s in self.accepting, states))
 
+    def determinize(self):
+        initial = ""
+        accepting = set()
+
+        # lista de conjuntos
+        states_to_add = [{self.initial}]
+        states_added = []
+
+        transitions = {}
+
+        while states_to_add:
+            # conjunto de estados a adicionar como unico estado
+            states = states_to_add.pop()
+
+            # para conjunto de estados, mapeia caracter para
+            # conjunto de estados resultantes
+            char_to_set = {}
+
+            accept = False
+            for state in states:
+                # eh estado de aceitacao se algum estado for
+                if state in self.accepting:
+                    accept = True
+
+                if state in self.transitions:
+                    for char, state_set in self.transitions[state].items():
+                        if char not in char_to_set:
+                            char_to_set[char] = state_set
+                        else:
+                            char_to_set[char] = \
+                                    char_to_set[char].union(state_set)
+
+            # estados novos precisam ser adicionados
+            for char, state_set in char_to_set.items():
+                if state_set not in states_added:
+                    states_to_add.append(state_set)
+
+            # transforma conjunto de estados em um estado (string)
+            char_to_state = {char : set_to_str(state_set) \
+                    for char, state_set in char_to_set.items()}
+            str_states = set_to_str(states)
+
+            # primeira transicao eh inicial
+            if not transitions:
+                initial = str_states
+
+            # se houver um estado de aceitacao, aceita
+            if accept:
+                accepting.add(str_states)
+
+            transitions[str_states] = char_to_state
+                    
+            # marca estado como ja especificado
+            states_added.append(states)
+
+        return DFA(transitions, initial, accepting)
--- tests/test_fa.py
+++ tests/test_fa.py
@@ -19,6 +19,12 @@ class TestFA(unittest.TestCase):
                 }
         self.ends_in_aa = NFA(transitions, "start", {"two a"})
 
+        self.no_aaa_accept = ["", "a", "aa", "baa", "aabbaa", "bababaab"]
+        self.no_aaa_reject = ["aaaba", "bbbbaaab", "abaaa", "baaa", "aaa"] 
+        self.ends_in_aa_accept = ["babaa", "baa", "aaaa",
+                "aa", "bababaa", "aabaa"]
+        self.ends_in_aa_reject = ["baba", "aba", "aaba",
+                "baaaaba", "bbbb", "aaab"]
     def check_strings(self, fa, accept, reject):
         for word in accept:
             self.assertTrue(fa.accepts(word))
@@ -26,20 +32,21 @@ class TestFA(unittest.TestCase):
             self.assertFalse(fa.accepts(word))
 
     def test_dfa_accept(self):
-        self.check_strings(self.no_aaa, 
-                ["", "a", "aa", "baa", "aabbaa", "bababaab"],
-                ["aaaba", "bbbbaaab", "abaaa", "baaa", "aaa"] 
-                )
+        self.check_strings(self.no_aaa, self.no_aaa_accept,
+                self.no_aaa_reject)
 
     def test_dfa_strings_of_size(self):
         three = {"aab", "aba", "abb", "baa", "bab", "bba", "bbb"}
         self.assertEqual(self.no_aaa.words_of_size(3), three)
 
     def test_nfa_accepts(self):
-        self.check_strings(self.ends_in_aa, 
-                ["babaa", "baa", "aaaa", "aa", "bababaa", "aabaa"],
-                ["baba", "aba", "aaba", "baaaaba", "bbbb", "aaab"]
-                )
+        self.check_strings(self.ends_in_aa, self.ends_in_aa_accept,
+                self.ends_in_aa_reject)
+
+    def test_determinize(self):
+        det = self.ends_in_aa.determinize()
+        self.check_strings(det, self.ends_in_aa_accept,
+                self.ends_in_aa_reject)
 
 if __name__ == "__main__":
     unittest.main()
