bdd1c75f6d9536cdf79b4a19c6dec034cd262e6f
U-DESKTOP-I3ASOEK\gabri <gabrielmullerw@gmail.com>
2018-05-29 00:21:58 -0300

    Costura, em-ordem

--- regex.py
+++ regex.py
@@ -4,27 +4,59 @@ class Node:
         self.right = None
         self.left = None
 
+        # filho da direita está acima?
+        self.up = False
+
     # Nodo é um operador?
     def is_operator(self):
         return self.value in Regex.up
 
+    # costura esse nodo com um nodo acima
+    def thread(self, up_node):
+        self.up = True
+        self.right = up_node
+
+    def in_order(self):
+        l = self.left.in_order() if self.left else []
+        r = self.right.in_order() if self.right else []
+        return l + [self] + r
+
 class Regex:
-    # semantics
-    up = \
+    # Semântica dos operadores
+    down = \
     {'|': 'or',
-    '.': 'concat',
+    '.': lambda node: {node.left},
     '?': 'interro',
     '*': 'kleene'}
 
-    down = \
+    up = \
     {'|': 'or',
-    '.': 'concat',
+    '.': lambda node: {node.right},
     '?': 'interro',
     '*': 'kleene'}
+
     # faz parse da string para criar a árvore de Simone
     # por enquanto apenas teste
     def __init__(self, regex_str):
         return
 
+    # costura toda árvore
+    def thread(self):
+        sequence = self.in_order()
+        parent = None
+
+        for node in sequence:
+           if parent:
+                parent.thread(node)
+
+           parent = None if node.right else node
+
+        if parent:
+            parent.thread('&')
+
+    def in_order(self):
+        return self.root.in_order()
+
+        
     def simone(self):
         return
--- tests/test_regex.py
+++ tests/test_regex.py
@@ -3,12 +3,22 @@ from regex import Regex, Node
 
 class TestRegex(unittest.TestCase):
     def setUp(self):
+        # árvore de strings para teste
+        self.tree = Node('.')
+        self.tree.left = Node('l')
+        self.tree.right = Node('r')
+        self.tree.right.left = Node('a')
+        self.tree.right.right = Node('b')
+        self.tree.left.left = Node('c')
+
         # 'ab' apenas
         self.ab = Regex('hello')
 
         self.ab.root = Node('.')
         self.ab.root.left = Node('a')
         self.ab.root.right = Node('b')
+
+        self.ab.thread()
 
         # (ab | ac)* a
         self.abaca = Regex('oi')
@@ -19,30 +29,33 @@ class TestRegex(unittest.TestCase):
         n.left.left = Node('a')
         n.left.right = Node('b')
         n.right.left = Node('a')
-        n.right.right = Node('b')
+        n.right.right = Node('c')
 
         r = Node('.')
         r.left = Node('*')
         r.right = Node('a')
         r.left.left = n
 
-        r.left.right = r
-        n.left.left.right = n.left
-        n.left.right.right = n
-        n.right.left.right = n.right
-        n.right.right.right = r.left
-        r.right.right = '&'
-
         self.abaca.root = r
+
+        self.abaca.thread()
 
     def test_is_operator(self):
         self.assertTrue(self.ab.root.is_operator())
         self.assertFalse(self.ab.root.left.is_operator())
         self.assertFalse(self.ab.root.right.is_operator())
+        self.assertFalse(self.ab.root.left.right.left.right.left.is_operator())
         
         self.assertTrue(self.abaca.root.left.left.left.is_operator())
         self.assertTrue(self.abaca.root.left.right.left.right.is_operator())
         self.assertFalse(self.abaca.root.right.is_operator())
+
+    def test_in_order(self):
+        nodes = self.tree.in_order()
+        vals = map(lambda node: node.value, nodes)
+        string = ''.join(vals)
+        self.assertEqual(string, "cl.arb")
+
 
 if __name__ == "__main__":
     unittest.main()
