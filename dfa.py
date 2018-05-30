75c90c6b17716773a9689e947cfe8f63b92ad66d
Juliana Pinheiro <julianas.pinheiro@outlook.com>
2018-05-30 14:14:34 -0300

    Remover estados inacessíveis e estados mortos

--- dfa.py
+++ dfa.py
@@ -31,3 +31,64 @@ class DFA:
         valid = lambda i : i[1] in self.accepting
         return {i[0] for i in filter(valid, words)}
 
+    """
+        Remover estado
+    """
+    def remove_state(self, removed):
+        # Remover estado
+        del self.transitions[removed]
+
+        # Remover transições para o estado removido
+        # TODO: melhorar. Talvez falhe em alguns casos
+        transitions_to_remove = {}
+        for state in self.transitions.keys():
+            print("state",state)
+            for key, next_state in self.transitions[state].items():
+                print("key", key, ", next", next_state)
+                if next_state == removed:
+                    print(next_state)
+                    transitions_to_remove[state] = key
+
+        for state,symbol in transitions_to_remove.items():
+            del self.transitions[state][symbol]
+
+    """
+        Remover estados inacessíveis
+    """
+    def remove_unreachable(self):
+        reacheable = set()
+        new_states = {self.initial}
+        while not new_states <= reacheable:
+            reacheable = reacheable | new_states
+            temp = new_states.copy()
+            new_states = set()
+            for state in temp:
+                for symbol in self.transitions[state]:
+                    new_states.add(self.transitions.get(state).get(symbol))
+        for unreachable in self.transitions.keys() - reacheable:
+            self.remove_state(unreachable)
+
+    """
+        Remover estados mortos
+    """
+    def remove_dead(self):
+        alive = set()
+        new_states = self.accepting.copy()
+        while not new_states <= alive:
+            alive = alive | new_states
+            new_states = set()
+            for state in self.transitions.keys():
+                for next_state in self.transitions[state].values():
+                    if(next_state in alive):
+                        new_states.add(state)
+        for dead in self.transitions.keys() - alive:
+            self.remove_state(dead)
+
+    """
+        Minimização de AF
+    """
+    def minimize(self):
+        self.remove_unreachable()
+        self.remove_dead()
+        #self.merge_nondistinguishable()
+
