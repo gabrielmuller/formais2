7360efac086b0d8538eadf03f36013348d6f2194
Juliana Pinheiro <julianas.pinheiro@outlook.com>
2018-05-31 15:40:39 -0300

    Concatenação e fechamento de GR

--- rg.py
+++ rg.py
@@ -102,6 +102,10 @@ class RegularGrammar():
                     productions[vn+str].add(i+str)
         return productions
 
+    """
+        União de duas GR
+        Retorna uma GR nova
+    """
     @staticmethod
     def union(grammar_a, grammar_b):
         """
@@ -128,6 +132,91 @@ class RegularGrammar():
 
         return RegularGrammar(initial, productions)
         
+    """
+        Concatenação de duas GR
+        Retorna uma GR nova
+    """
+    @staticmethod
+    def concatenation(grammar_a, grammar_b):
+        """
+            1 - Adicionar produções com não-terminais renomeados
+            para evitar não-terminais diferentes com mesmo
+            nome
+        """
+        a = grammar_a._rename_productions("1")
+        b = grammar_b._rename_productions("2")
+        productions = {**a, **b}
+
+        """
+            Símbolos iniciais S1 e S2
+        """
+        initial1 = grammar_a.initial+"1"
+        initial2 = grammar_b.initial+"2"
+
+        """
+            2 - Para toda regra A->a em GR B, 
+            substitui por A->aS2 onde S2 é o símbolo inicial
+            de GR B renomeado
+        """
+        for vn, values in a.items(): 
+            for i in values:
+                if len(i) == 1 and i != "&":
+                    productions[vn].remove(i)
+                    productions[vn].add(i+initial2)
+
+        """
+            3 - Se "S1->&" pertence as produções de GR A,
+            replica as produções de S2 em S1.
+        """
+        if "&" in a[initial1]:
+            for production in productions[initial2]:
+                productions[initial1].add(production)
+
+        """
+            4 - Se "S2->&" pertence às produções de GR B, 
+            retira a produção da GR final e adiciona a produção
+            "S1->&" (onde S1 é o estado inicial da GR final)
+        """
+        if "&" in productions[initial2]:
+            productions[initial2].pop("&")
+            productions[initial1].add("&")
+        
+        return RegularGrammar(initial1, productions)
+
+    """
+        Fechamento de uma GR
+        Retorna uma GR nova
+    """
+    @staticmethod
+    def kleene_closure(grammar):
+        """
+            1 - Remover &
+        """
+        productions = grammar.productions
+        initial = grammar.initial
+        productions[initial].remove("&")
+
+        """
+            2 - Para toda regra A->a em G, 
+            substitui por A->aS onde S é o símbolo inicial de G
+        """
+        for vn, values in productions.items():
+            for i in values:
+                if len(i) == 1 and i != "&":
+                    productions[vn].remove(i)
+                    productions[vn].add(i+initial)
+
+        """
+            3 - Se "S->&" pertence as produções de G,
+            cria novo estado inicial S' e S'->&
+        """
+        initial += "'"
+        productions[initial] = {"&"}
+        for production in productions[grammar.initial]:
+            productions[initial].add(production)
+       
+        return RegularGrammar(initial, productions)
+
 
 
 
